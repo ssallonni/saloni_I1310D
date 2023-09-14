7a922457c76629f885eab6394920df67da42c46c
ssallonni <143915566+ssallonni@users.noreply.github.com>
2023-09-14 16:38:58 -0500

    Update volume_of_sphere.py

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -1,7 +1,7 @@
 def calculate_volume_of_sphere(radius)
-  pi = 3.14
-	area = pi * radius * radius
-	return area
+	pi = 3.14
+	volume = 4 * pi * (radius**2)
+	return volume
 r1 = 30
 r2 = 40
 print(calculate_colume_of_sphere(r1))
